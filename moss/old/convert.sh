7007107038e859826e9f709599fab639195c998c
Ryan Patrick <3922408+ryanspatrick@users.noreply.github.com>
2022-12-01 12:02:55 -0600

    Add files via upload

---
+++ moss/old/convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file=$1
+d=`dirname ${file}`
+b=`basename ${file} .html`
+
+grep -e \% -e right\>[0-9]*$ ${file} | sed 's@^.*<TD><A HREF=[^>]*>\([^ ]*\)[^(]*.\([^%]*\).*@\1,\2@g;s/<TD ALIGN=right>//g' > ${file}.temp
+export CLASSPATH="${d}/java/antlr-4.5.3-complete.jar:${d}/java"
+java -Xmx2048m org.antlr.v4.gui.TestRig MOSS file ${file}.temp 1> ${d}/${b}.csv 2> ${d}/${b}.err
+sed "s/DATE/${b}/g" ${d}/template.plt > ${d}/${b}.plt
+gnuplot ${d}/${b}.plt
+epstopdf --outfile=${d}/${b}.pdf ${d}/${b}.eps
+chmod a+r ${d}/${b}.pdf
+
+rm `ls ${d}/${b}*.* | grep -v -e pdf$`
